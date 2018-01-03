35fbce8df0e92434e033b7afaafc1cc4122caf6d
Sebastiano Roncato <crowmaster9281@gmail.com>
2018-01-03 20:28:52 +0100

    Tested and fixed image size and text position

--- moebot_bot/util/util.go
+++ moebot_bot/util/util.go
@@ -6,6 +6,7 @@ import (
 	"image"
 	"image/color"
 	"image/gif"
+	"math"
 	"regexp"
 	"strings"
 
@@ -103,18 +104,19 @@ func GetSpoilerContents(messageParams []string) (title string, text string) {
 		return "", ""
 	}
 	reg := regexp.MustCompile("^(\\[.+?\\])")
-	return strings.Replace(reg.FindString(strings.Join(messageParams, " ")), "]", "", 2), reg.ReplaceAllString(strings.Join(messageParams, " "), "")
+	return strings.Replace(strings.Replace(reg.FindString(strings.Join(messageParams, " ")), "]", "", 1), "[", "", 1), reg.ReplaceAllString(strings.Join(messageParams, " "), "")
 }
 
 func MakeGif(text string) []byte {
 	var images []*image.Paletted
-	img, _ := UniformColorImage(image.Rect(0, 0, font.MeasureString(basicfont.Face7x13, text).Ceil()+100, 100),
-		color.RGBA{0x00, 0x00, 0x00, 0xff}, fixed.Point26_6{fixed.Int26_6(1 * 64), fixed.Int26_6(1 * 64)})
+	img, d := UniformColorImage(image.Rect(0, 0, int(math.Max(100, float64(font.MeasureString(basicfont.Face7x13, text).Ceil())))+30, 30),
+		color.RGBA{0xff, 0xff, 0xff, 0xff}, color.RGBA{0x00, 0x00, 0x00, 0xff}, fixed.Point26_6{fixed.Int26_6(10 * 64), fixed.Int26_6(16 * 64)})
+	d.DrawString("Hover to view")
 	images = append(images, img)
 
 	for i := 0; i < 10; i++ {
-		img, d := UniformColorImage(image.Rect(0, 0, font.MeasureString(basicfont.Face7x13, text).Ceil()+100, 100),
-			color.RGBA{0xff, 0xff, 0xff, 0xff}, fixed.Point26_6{fixed.Int26_6(1 * 64), fixed.Int26_6(1 * 64)})
+		img, d := UniformColorImage(image.Rect(0, 0, int(math.Max(100, float64(font.MeasureString(basicfont.Face7x13, text).Ceil())))+30, 30),
+			color.RGBA{0x00, 0x00, 0x00, 0xff}, color.RGBA{0xff, 0xff, 0xff, 0xff}, fixed.Point26_6{fixed.Int26_6(10 * 64), fixed.Int26_6(16 * 64)})
 		d.DrawString(text)
 		images = append(images, img)
 	}
@@ -123,22 +125,32 @@ func MakeGif(text string) []byte {
 	gif.EncodeAll(buf, &gif.GIF{
 		Image:     images,
 		LoopCount: 1,
-		Delay:     []int{5, 5, 5, 5, 5, 5, 5, 5, 5, 5},
+		Delay:     []int{500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500},
 	})
 	return buf.Bytes()
 }
 
-func UniformColorImage(size image.Rectangle, c color.RGBA, startPoint fixed.Point26_6) (result *image.Paletted, drawer *font.Drawer) {
+func UniformColorImage(size image.Rectangle, imageColor color.RGBA, textColor color.RGBA, startPoint fixed.Point26_6) (result *image.Paletted, drawer *font.Drawer) {
 	var palette = []color.Color{
 		color.RGBA{0x00, 0x00, 0x00, 0xff},
 		color.RGBA{0xff, 0xff, 0xff, 0xff},
 	}
 	img := image.NewPaletted(size, palette)
+	SetBackground(img, imageColor)
 	d := &font.Drawer{
 		Dst:  img,
-		Src:  image.NewUniform(c),
+		Src:  image.NewUniform(textColor),
 		Face: basicfont.Face7x13,
 		Dot:  startPoint,
 	}
 	return img, d
+}
+
+func SetBackground(img *image.Paletted, imgColor color.RGBA) {
+	bounds := img.Bounds()
+	for x := 0; x < bounds.Size().X; x++ {
+		for y := 0; y < bounds.Size().Y; y++ {
+			img.Set(x, y, imgColor)
+		}
+	}
 }
