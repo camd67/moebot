462cb28a16fcfba4eba1ce3b0f6f4188b5770c6c
CamD67 <cameron.doane@gmail.com>
2018-01-13 16:33:11 -0800

    fix merge conflicts

--- moebot_bot/bot/help.go
+++ moebot_bot/bot/help.go
@@ -3,7 +3,7 @@ package bot
 func commHelp(pack *commPackage) {
 	if len(pack.params) == 0 {
 		pack.session.ChannelMessageSend(pack.channel.ID, "Moebot has the following commands:\n"+
-			"`"+ComPrefix+" team <role name>` - Changes your role to one of the approved roles. `"+ComPrefix+" team` to list all teams\n"+
+			"`"+ComPrefix+" team <team name>` - Changes your team to one of the approved teams. `"+ComPrefix+" team` to list all teams\n"+
 			"`"+ComPrefix+" rank <rank name>` - Changes your rank to one of the approved ranks. `"+ComPrefix+" rank` to list all the ranks\n"+
 			"`"+ComPrefix+" changelog` - Displays the changelog for moebot\n"+
 			"`"+ComPrefix+" NSFW` - Gives you NSFW channel permissions\n"+
@@ -11,6 +11,7 @@ func commHelp(pack *commPackage) {
 			"`"+ComPrefix+" permit <perm level> <role name>` - Master/All only. Grants permission to the selected role.\n"+
 			"`"+ComPrefix+" custom <command name> <role name>` - Master/All/Mod Links up a role to be toggable by the command name. Type `"+ComPrefix+" role <command name> to toggle`\n"+
 			"`"+ComPrefix+" poll -options <option 1, option 2, option 3, ...> -title <poll title>` - Master/All/Mod set up a poll with the given options. Type `"+ComPrefix+" poll -close <poll id> to close`\n"+
+			"`"+ComPrefix+" role <role name>` - Changes your role to one of the approved roles. `"+ComPrefix+" role` to list all the roles\n"+
 			"`"+ComPrefix+" help` - Displays this message")
 	}
 }
