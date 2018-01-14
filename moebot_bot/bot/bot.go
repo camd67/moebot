9b0ea95f8b050087b7d00b55225ae744994e97ed
CamD67 <cameron.doane@gmail.com>
2018-01-13 17:51:18 -0800

    update bot version and changelog

--- moebot_bot/bot/bot.go
+++ moebot_bot/bot/bot.go
@@ -15,7 +15,7 @@ import (
 )
 
 const (
-	version = "0.2.3"
+	version = "0.2.4"
 )
 
 type commPackage struct {
--- moebot_bot/bot/changeLog.go
+++ moebot_bot/bot/changeLog.go
@@ -4,22 +4,26 @@ const changeLogPrefix = "\n`->` "
 
 // probably want to move this to the DB, but not bad to have it here
 var changeLog = map[string]string{
+
+	"0.2.4": changeLogPrefix + "Added spoiler command" +
+		changeLogPrefix + "Added poll command",
+
+	"0.2.3": changeLogPrefix + "Added ping command" +
+		changeLogPrefix + "Added permit command" +
+		changeLogPrefix + "Added custom command",
+
+	"0.2.2": changeLogPrefix + "Added echo command for master only" +
+		changeLogPrefix + "added `raffle winner` and `raffle count` to get the raffle winner and vote counts" +
+		changeLogPrefix + "removed ticket generation",
+
+	"0.2.1": changeLogPrefix + "Updated raffle art/relic submissions to post all submissions on command instead of over time.",
+
 	"0.2": changeLogPrefix + "Included this command!" +
 		changeLogPrefix + "Updated `Rank` command to prevent removal of lowest role." +
 		changeLogPrefix + "Added random drops for tickets" +
 		changeLogPrefix + "Fixed the cooldown so users wouldn't be spammed due to high luck stat" +
 		changeLogPrefix + "Added `Raffle` related commands... For rafflin'" +
 		changeLogPrefix + "For future reference, previous versions included help, team, rank, and NSFW commands as well as a welcome message to the server.",
-
-	"0.2.1": changeLogPrefix + "Updated raffle art/relic submissions to post all submissions on command instead of over time.",
-
-	"0.2.2": changeLogPrefix + "Added echo command for master only" +
-		changeLogPrefix + "added `raffle winner` and `raffle count` to get the raffle winner and vote counts" +
-		changeLogPrefix + "removed ticket generation",
-
-	"0.2.3": changeLogPrefix + "Added ping command" +
-		changeLogPrefix + "Added permit command" +
-		changeLogPrefix + "Added custom command",
 }
 
 func commChange(pack *commPackage) {
