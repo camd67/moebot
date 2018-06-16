fef0e742535b548ecff31815e308a23fff386789
efevans <efevans1@gmail.com>
2018-06-15 19:29:52 -0700

    Add RWMutex to channel map

--- moebot_bot/bot/commands/timer.go
+++ moebot_bot/bot/commands/timer.go
@@ -3,20 +3,25 @@ package commands
 import (
 	"fmt"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/camd67/moebot/moebot_bot/bot/permissions"
+	"github.com/camd67/moebot/moebot_bot/util"
 	"github.com/camd67/moebot/moebot_bot/util/db"
 )
 
 type TimerCommand struct {
-	chTimers map[string]time.Time
+	chTimers util.SyncChannelTimerMap
 	Checker  permissions.PermissionChecker
 }
 
 func NewTimerCommand() *TimerCommand {
 	tc := &TimerCommand{}
-	tc.chTimers = map[string]time.Time{}
+	tc.chTimers = util.SyncChannelTimerMap{
+		RWMutex: sync.RWMutex{},
+		M:       make(map[string]time.Time),
+	}
 	return tc
 }
 
@@ -24,20 +29,25 @@ func (tc *TimerCommand) Execute(pack *CommPackage) {
 	channelID := pack.message.ChannelID
 	if len(pack.params) > 0 && strings.EqualFold(pack.params[0], "start") {
 		if tc.Checker.HasPermission(pack.message.Author.ID, pack.member.Roles, pack.guild, db.PermMod) {
-			tc.chTimers[channelID] = time.Now()
+			tc.chTimers.Lock()
+			tc.chTimers.M[channelID] = time.Now()
+			tc.chTimers.Unlock()
 			pack.session.ChannelMessageSend(pack.message.ChannelID, "Timer started! OwO")
 		} else {
 			pack.session.ChannelMessageSend(pack.message.ChannelID, pack.message.Author.Mention()+", you... you don't have permission to do that! ( ≧Д≦)")
 		}
 	} else {
-		if v, ok := tc.chTimers[channelID]; ok {
+		tc.chTimers.Lock()
+		if v, ok := tc.chTimers.M[channelID]; ok {
 			pack.session.ChannelMessageSend(pack.message.ChannelID, fmtDuration(time.Since(v)))
 		} else {
 			pack.session.ChannelMessageSend(pack.message.ChannelID, "No timer started for this channel... -w-")
 		}
+		tc.chTimers.Unlock()
 	}
 }
 
+// fmtDuration formats a duration into a hh:mm:ss format
 func fmtDuration(dur time.Duration) string {
 	remainingDur := dur.Round(time.Second)
 	hours := remainingDur / time.Hour
