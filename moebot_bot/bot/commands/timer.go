8e2759bcbb31da1f98410f404a1a3d10d176b460
efevans <efevans1@gmail.com>
2018-06-21 21:45:03 -0700

    Improve the help text

--- moebot_bot/bot/commands/timer.go
+++ moebot_bot/bot/commands/timer.go
@@ -133,7 +133,7 @@ func (tc *TimerCommand) GetCommandKeys() []string {
 }
 
 func (tc *TimerCommand) GetCommandHelp(commPrefix string) string {
-	return fmt.Sprintf("`%[1]s timer` - Checks the timestamp. Moderators may provide the `--start` option to begin start or restart the timer.", commPrefix)
+	return fmt.Sprintf("`%[1]s timer` - Checks the timer. Moderators may provide the `start` option to start (or restart) the timer.", commPrefix)
 }
 
 type SyncChannelTimerMap struct {
