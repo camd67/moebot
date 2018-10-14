198172ecec0c0cbea6fbe7c412f0aa7fa9703c7c
CamD67 <7773345+camd67@users.noreply.github.com>
2018-10-14 12:47:07 -0700

    Disable inserting metric data into the db

--- moebot_bot/bot/bot.go
+++ moebot_bot/bot/bot.go
@@ -281,7 +281,8 @@ func messageCreate(session *discordgo.Session, message *discordgo.MessageCreate)
 		runCommand(session, message.Message, guild, channel, member, &userProfile, &timer)
 	}
 	// In this case we don't care about the error state as the user doesn't need to know we failed to serialize the metric and we already logged it
-	db.MetricInsertTimer(timer, userProfile)
+	// Disabled for now as we're getting incorrect information in the db...
+	//db.MetricInsertTimer(timer, userProfile)
 
 	// make sure to also check if they agreed to the rules
 	if isNewUser {
