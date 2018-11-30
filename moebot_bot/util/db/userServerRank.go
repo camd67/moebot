60ac9ac33fea1734277acb1c926c0e60fb857b3c
efevans <efevans1@gmail.com>
2018-11-29 20:54:25 -0800

    removed unused sql constants

--- moebot_bot/util/db/userServerRank.go
+++ moebot_bot/util/db/userServerRank.go
@@ -11,25 +11,6 @@ import (
 	"github.com/volatiletech/sqlboiler/queries/qm"
 )
 
-const (
-	userServerRankTable = `CREATE TABLE IF NOT EXISTS user_server_rank(
-		Id SERIAL NOT NULL PRIMARY KEY,
-		ServerId INTEGER NOT NULL REFERENCES server(id) ON DELETE CASCADE,
-		UserId INTEGER NOT NULL REFERENCES user_profile(id) ON DELETE CASCADE,
-		Rank INTEGER NOT NULL DEFAULT 0,
-		MessageSent BOOLEAN NOT NULL DEFAULT false
-	)`
-
-	userServerRankQuery = `SELECT user_server_rank.Id, user_server_rank.ServerId, user_server_rank.UserId, user_server_rank.Rank, user_server_rank.MessageSent FROM user_server_rank
-		JOIN server ON server.Id = user_server_rank.ServerId
-		JOIN user_profile ON user_profile.Id = user_server_rank.UserId
-		WHERE server.GuildUid = $1 AND user_profile.UserUid = $2`
-	userServerRankQueryId       = `SELECT Id, Rank, MessageSent FROM user_server_rank WHERE ServerId = $1 AND UserId = $2`
-	userServerRankUpdate        = `UPDATE user_server_rank SET Rank = Rank + $2 WHERE Id = $1 RETURNING user_server_rank.Id, user_server_rank.Rank, user_server_rank.MessageSent`
-	userServerRankInsert        = `INSERT INTO user_server_rank(ServerId, UserId, Rank) VALUES ($1, $2, $3) RETURNING user_server_rank.Id, user_server_rank.Rank, user_server_rank.MessageSent`
-	userServerRankUpdateMessage = `UPDATE user_server_rank SET MessageSent = true WHERE Id = ANY ($1::integer[])`
-)
-
 func UserServerRankQuery(userUid string, guildUid string) (usr *models.UserServerRank, err error) {
 	return models.UserServerRanks(
 		qm.InnerJoin("server s on s.id = user_server_rank.server_id"),
